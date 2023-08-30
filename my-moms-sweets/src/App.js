ef475f9e1dc325706e5c0a960ed37d2eac70c044
Suhayb Walton <s3842119@student.rmit.edu.au>
2023-08-30 13:55:30 +1000

    Further enhancements

--- my-moms-sweets/src/App.js
+++ my-moms-sweets/src/App.js
@@ -2,36 +2,36 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
-import Header from './components/Header'; 
-import NavBar from './components/NavBar';  
+import Header from './components/Header';  // Importing Header
+import NavBar from './components/NavBar';  // Importing NavBar
 import HeroSection from './components/HeroSection';  
 import AboutSection from './components/AboutSection';
 import HowToOrderSection from './components/HowToOrderSection'; 
 import Footer from './components/Footer';  
 import ContactSection from './components/ContactSection'; 
-import FadeInSection from './components/FadeInSection';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <NavBar />
+      <NavBar />  {/* Your NavBar here */}
 
       <div id="heroSection">
         <HeroSection />
       </div>
 
-      <FadeInSection>
+      <div id="aboutSection">
         <AboutSection />
-      </FadeInSection>
-
-      <FadeInSection>
+      </div>
+      
+      <div id="howToOrderSection">
         <HowToOrderSection />
-      </FadeInSection>
+      </div>
 
-      <FadeInSection>
+      <div id="contactSection">
         <ContactSection />
-      </FadeInSection>
+      </div>
 
       <Footer />
     </div>
--- my-moms-sweets/src/components/HeroSection.js
+++ my-moms-sweets/src/components/HeroSection.js
@@ -1,21 +1,12 @@
-import React, { useEffect } from 'react';
-import heroImage from '../images/hero-image.jpg';
+import React from 'react';
+import './HeroSection.css'; 
+import heroImage from '../images/hero-image.jpg';  
+
 
 const HeroSection = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      const parallax = document.getElementById('heroSection');
-      let scrollPosition = window.pageYOffset;
-      parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <div id="heroSection" style={{ backgroundAttachment: 'fixed' }}>
-      <img src={heroImage} alt="Traditional Somali Sweets" className="hero-image" />
+    <div className="hero-container">
+      <img src={heroImage} alt="Traditional Somali Sweets" className="hero-image" />  
       <div className="hero-content">
         <h1>Welcome to Safiyo's Traditional Somali Halwa Sweets!</h1>
         <p>Experience the taste of authentic Somali Halwa, made with love.</p>
@@ -25,3 +16,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
